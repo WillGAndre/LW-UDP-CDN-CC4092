a562804828a4eb78d2a318b707790cde71b3953f
WillGAndre <xunga@mail.com>
2023-03-24 13:42:30 +0000

    stable release loadbalancer / Client integration

--- lb_nginx/app/app.py
+++ lb_nginx/app/app.py
@@ -1,13 +1,17 @@
 import socket
 from flask import Flask
+from pymemcache.client import base
 
 app = Flask(__name__)
+client = base.Client(('memcached', 11211))
+client.set('token', '123')
 
+# Jinja2
 # Route for the default page
 @app.route("/")
 def home():
     # Display message
-    html = "<center><h3>Flask-NGINX-LD-TEST</h3></center>"
+    html = f"<center><h3>Flask-NGINX-LD-TEST-TOKEN-{client.get('token')}</h3></center>"
     return html.format(hostname=socket.gethostname())
 
 if __name__ == '__main__':
--- lb_nginx/app/wsgi.py
+++ lb_nginx/app/wsgi.py
@@ -1,9 +1,3 @@
 from app import app
-from applib import applib
-
-result = applib.multiply(2,3)
-print("result")
-print(result)
-# Run caching (?)
 if __name__ == "__main__":
     app.run()
