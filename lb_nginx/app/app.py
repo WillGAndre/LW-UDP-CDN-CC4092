a0bb72e0f289d134932a2832dab523f9ab9ca164
WillGAndre <xunga@mail.com>
2023-04-14 16:33:34 +0100

    Set todo / lb changes

--- lb_nginx/app/app.py
+++ lb_nginx/app/app.py
@@ -1,10 +1,31 @@
 import socket
+from logging.config import dictConfig
 from flask import Flask
 from pymemcache.client import base
+from clientListener import Listener
 
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['wsgi']
+    }
+})
 app = Flask(__name__)
+cl_listener = Listener(9000, 1024, app.logger)
+cl_listener.listen()
+
+## Basic Cache example ---
 client = base.Client(('memcached', 11211))
 client.set('token', '123')
+# ---
 
 # Jinja2
 # Route for the default page
