363af6137cac927b8a0af710c58a9522a7d471a5
WillGAndre <xunga@mail.com>
2023-05-19 17:21:45 +0100

    iter03 node test

--- gcloud_internal_ring/node/node.py
+++ gcloud_internal_ring/node/node.py
@@ -1,4 +1,16 @@
 GCLOUD_CREDS = "asc23-378811-ca4948da7594.json"
 
-if __name__ == "__main__":
-    print("internal node hit")
+import os
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def homepage():
+    if request.method == "GET":
+        return jsonify({"message": "Hello World!"})
+
+PORT = int(os.environ.get("PORT", 8080))
+if __name__ == '__main__':
+    print("starting internal node...")
+    app.run(threaded=True,host='0.0.0.0',port=PORT)
